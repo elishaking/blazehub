d2ae1a624834748db9c73f70e5f2a654fe64736e
King Elisha <ek.chibueze@gmail.com>
2019-10-15 16:57:07 +0100

    signin route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const firebase = require('firebase');
 require('firebase/database');
 
 const validateSignupData = require('../../validation/signup');
+const validateSigninData = require('../../validation/signin');
 
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
@@ -59,6 +61,59 @@ router.post("/signup", (req, res) => {
         });
       });
     });
+});
+
+//@route POST /api/users/signin
+//@description Authenticate user
+//@access Public
+router.post('/signin', (req, res) => {
+  const { isValid, errors } = validateSigninData(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const userKey = email.replace(/\./g, "~").replace(/@/g, "~~");
+  const userRef = dbRef.child('users').child(userKey);
+
+  userRef.once('value', (dataSnapshot) => {
+    if (!dataSnapshot.exists()) {
+      errors.email = "No user with this email, Please Sign Up";
+      return res.status(400).json(errors);
+    }
+
+    const user = dataSnapshot.val();
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // JWT payload
+        const jwtPayload = {
+          id: userKey,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email
+        };
+
+        // Sign Token <==> encodes payload into token
+        jwt.sign(
+          jwtPayload,
+          process.env.SECRET_OR_KEY,
+          {
+            expiresIn: 3600 * 24
+          },
+          (err, token) => res.json({
+            success: true,
+            token: `Bearer ${token}`
+          })
+        )
+      } else {
+        errors.password = 'Password incorrect';
+        res.status(400).json(errors);
+      }
+    });
+  })
 });
 
 module.exports = router;
