1feaa53fecaf41cb105404f828caa3bdd119ab1b
King Elisha <ek.chibueze@gmail.com>
2019-10-15 13:30:48 +0100

    basic route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
+import store from './store';
+
+import Landing from './components/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Route exact path="/" component={Landing} />
+      </Router>
+    </Provider>
   );
 }
 
---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Landing</h1>
+      </div>
+    )
+  }
+}
