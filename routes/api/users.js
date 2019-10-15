1393e8564d99a87000f0b049351f4f20fd5d6203
King Elisha <ek.chibueze@gmail.com>
2019-10-15 14:24:53 +0100

    environment variables and user route setup

---
+++ routes/api/users.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+// @route POST api/users/register
+// @description Register new user
+// @access Public
+router.post("/signup", (req, res) => {
+
+});
--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
+const dotenv = require('dotenv');
 
 const server = express();
 
 server.use(helmet());
 
 server.use(express.json());
-server.use(express.urlencoded({extended: false}));
+server.use(express.urlencoded({ extended: false }));
 
 server.use((req, res, next) => {
   console.log(`${Date().split(" ")[4]}: ${req.method} Request: ${req.originalUrl}`);
   next();
-}); 
+});
 
 server.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-server.listen(3000, () => {
+// load environment variables
+dotenv.config();
+
+server.listen(process.env.PORT, () => {
   console.log("Server online");
 });
