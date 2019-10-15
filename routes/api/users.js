7c6a3f6ef8a316103a4921daf37076ec714430fd
King Elisha <ek.chibueze@gmail.com>
2019-10-15 16:31:57 +0100

    signup user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,29 +31,34 @@ router.post("/signup", (req, res) => {
   const userEmail = req.body.email;
   const userKey = userEmail.replace(/\./g, "~").replace(/@/g, "~~");
 
-  const usersRef = dbRef.child('users');
-  usersRef
-    .once(userKey, (err) => {
-      if (err) {
-        console.error(err);
-        res.send("error");
+  const userRef = dbRef.child('users').child(userKey);
+  userRef
+    .once('value', (dataSnapshot) => {
+      if (dataSnapshot.exists()) {
+        errors.email = "Email already exists";
+        return res.status(400).json(errors);
       }
-    });
-  // usersRef
-  //   .child(userKey)
-  //   .set(userEmail, (err) => {
-  //     if (err) {
-  //       errors.email = "Email already exists";
-  //       return res.status(400).json(errors);
-  //     }
 
-  //     bcrypt.genSalt(10, (err, salt) => {
-  //       if (err) console.error(err);
-  //       bcrypt.hash(req.body.password, salt, (err, hash) => {
-  //         if (err) console.error(err);
-  //       });
-  //     });
-  //   })
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) console.error(err);
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+          if (err) return console.error(err);
+
+          const newUser = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: hash,
+          }
+          userRef
+            .set(newUser, (err) => {
+              if (err) return console.error(err);
+
+              res.json({ success: true });
+            });
+        });
+      });
+    });
 });
 
 module.exports = router;
