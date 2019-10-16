7573dfd8969329dcf6bede9ecd9a48437d9480fd
King Elisha <ek.chibueze@gmail.com>
2019-10-16 16:58:35 +0100

    navbar home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-export default class Home extends Component {
+class Home extends Component {
   render() {
+    const { firstName, lastName } = this.props.auth.user;
     return (
       <div className="container">
         <header>
-          <nav>
-            <h1>
+          <nav className="auth-nav">
+            <h1 className="logo">
               <img src={`./assets/img/logo-pri.svg`} alt="Logo" srcSet="" /> <span>BlazeChat</span>
             </h1>
 
-            <div>
+            <div className="search">
               <div className="icon-input">
                 <input type="text" />
                 <FontAwesomeIcon icon={faSearch} className="icon" />
               </div>
+            </div>
+
+            <div className="auth-nav-right">
+              <img src="" alt={firstName} srcset="" />
+              <span>{`${firstName} ${lastName}`}</span>
+              <input type="button" value="Sign Out" className="btn-input" />
             </div>
           </nav>
         </header>
@@ -26,3 +34,9 @@ export default class Home extends Component {
     )
   }
 }
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Home);
