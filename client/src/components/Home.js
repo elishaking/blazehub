ec32140d6a15e454c88e4dd5795eab7553f3c17c
King Elisha <ek.chibueze@gmail.com>
2019-10-16 16:22:58 +0100

    icon input

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
+      <div className="container">
+        <header>
+          <nav>
+            <h1>
+              <img src={`./assets/img/logo-pri.svg`} alt="Logo" srcSet="" /> <span>BlazeChat</span>
+            </h1>
+
+            <div>
+              <div className="icon-input">
+                <input type="text" />
+                <FontAwesomeIcon icon={faSearch} className="icon" />
+              </div>
+            </div>
+          </nav>
+        </header>
+
+
       </div>
     )
   }
