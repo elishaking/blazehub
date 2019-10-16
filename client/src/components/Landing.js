255581414c343563dab5cd03f54719ed268bcc17
King Elisha <ek.chibueze@gmail.com>
2019-10-16 08:07:09 +0100

    right

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -23,45 +23,51 @@ export default class Landing extends Component {
 
         <div className="content">
           <div className="left">
-            <div className="info">
-              <ul>
-                <li>
-                  <img src="" alt="" srcset="" />
-                  <p>Connect With Friends</p>
-                </li>
-                <li>
-                  <img src="" alt="" srcset="" />
-                  <p>Chat, Share Photos, Videos and more</p>
-                </li>
-                <li>
-                  <img src="" alt="" srcset="" />
-                  <p>Be a part of a growing community</p>
-                </li>
-              </ul>
+            <div className="inner">
+              <div className="info">
+                <ul>
+                  <li>
+                    <img src="./assets/img/connect.svg" alt="Connection" srcset="" />
+                    <p>Connect With Friends</p>
+                  </li>
+                  <li>
+                    <img src="./assets/img/converse.svg" alt="Conversation" srcset="" />
+                    <p>Chat, Share Photos, Videos and more</p>
+                  </li>
+                  <li>
+                    <img src="./assets/img/commune.svg" alt="Community" srcset="" />
+                    <p>Be a part of a growing community</p>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
 
           <div className="right">
-            <div className="welcome">
-              <img src="" alt="" srcset="" />
-              <h1>Join BlazeChat Today</h1>
-            </div>
-
-            <form action="/signup">
-              <div className="name">
-                <input type="text" name="firstName" id="firstName" placeholder="first name" />
-                <input type="text" name="lastName" id="lastName" placeholder="last name" />
+            <div className="inner">
+              <div className="welcome">
+                <img src={logo} alt="Logo" srcset="" />
+                <h1>Join BlazeChat Today</h1>
               </div>
-              <input type="email" name="email" id="email" placeholder="email" />
-              <input type="password" name="password" id="password" placeholder="password" />
-              <select name="gender" id="gender">
-                <option hidden disabled selected value="other">gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-              </select>
-              <input type="submit" value="Sign Up" className="btn-input" />
-            </form>
+
+              <form action="/signup">
+                <div>
+                  <div className="name">
+                    <input type="text" name="firstName" id="firstName" placeholder="first name" />
+                    <input type="text" name="lastName" id="lastName" placeholder="last name" />
+                  </div>
+                  <input type="email" name="email" id="email" placeholder="email" className="fill-parent" />
+                  <input type="password" name="password" id="password" placeholder="password" className="fill-parent" />
+                  <select name="gender" id="gender" className="fill-parent">
+                    <option hidden disabled selected value="other">gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+                <input type="submit" value="Sign Up" className="btn-input btn-pri" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
