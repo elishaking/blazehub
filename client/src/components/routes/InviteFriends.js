b877663891471d9338535e4174ff43dad650ee18
King Elisha <ek.chibueze@gmail.com>
2019-10-22 20:23:58 +0100

    ui

--- client/src/components/routes/InviteFriends.js
+++ client/src/components/routes/InviteFriends.js
@@ -4,6 +4,24 @@ import AuthNav from '../nav/AuthNav';
 import MainNav from '../nav/MainNav';
 
 class InviteFriends extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      friendEmails: [
+        { name: "" }
+      ]
+    };
+  }
+
+  addField = () => {
+    const { friendEmails } = this.state;
+    friendEmails.push({ name: "" });
+    this.setState({
+      friendEmails
+    });
+  }
+
   render() {
     const hasProfilePic = false;
     const { user } = this.props.auth;
@@ -16,7 +34,17 @@ class InviteFriends extends Component {
           <MainNav user={user} />
 
           <div className="invite-friends">
+            <form className="add-friend">
+              <h3>Invite your friends</h3>
 
+              {this.state.friendEmails.map((_, index) => (
+                <input type="text" name={`friend${index}`} placeholder="email" />
+              ))}
+
+              <button type="button" className="btn" onClick={this.addField}>Add Input</button>
+
+              <button type="submit" className="btn" onClick={this.addField} style={{ marginLeft: "auto" }}>Invite</button>
+            </form>
           </div>
         </div>
       </div>
