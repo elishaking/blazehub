28ff24bb632d7cb903ddf79e113c24d20704e829
King Elisha <ek.chibueze@gmail.com>
2019-10-25 17:46:35 +0100

    redux dev

--- client/src/store.js
+++ client/src/store.js
@@ -5,12 +5,14 @@ import rootReducer from './redux_reducers';
 const initialGlobalState = {};
 const middleware = [thunk];
 
+const reduxExt = process.env.NODE_ENV == "development" ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null;
+
 const store = createStore(
   rootReducer,
   initialGlobalState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reduxExt
   )
 );
 
