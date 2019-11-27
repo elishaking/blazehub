dccadd308cd304bd17dcf2163b998a64bfa47d4b
King Elisha <ek.chibueze@gmail.com>
2019-11-27 09:10:30 +0100

    unused funcs

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -88,47 +88,6 @@ export default class Post extends Component {
     });
   }
 
-  resizeImage = (dataUrl, type) => {
-    const img = document.createElement("img");
-    img.src = dataUrl;
-    return new Promise((resolve, reject) => {
-      img.onload = () => {
-        // console.log(img.height);
-        const canvas = document.createElement('canvas');
-        const max = img.height > img.width ? img.height : img.width;
-        if (max > 1000) {
-          canvas.height = (img.height / max) * 1000;
-          canvas.width = (img.width / max) * 1000;
-
-          const context = canvas.getContext('2d');
-          context.scale(1000 / max, 1000 / max);
-          context.drawImage(img, 0, 0);
-          // return canvas.toDataURL();
-          resolve(canvas.toDataURL(type, 0.5));
-        } else {
-          // return dataUrl;
-          resolve(dataUrl);
-        }
-      }
-    });
-  };
-
-  base64MimeType = (encoded) => {
-    var result = null;
-
-    if (typeof encoded !== 'string') {
-      return result;
-    }
-
-    var mime = encoded.match(/data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/);
-
-    if (mime && mime.length) {
-      result = mime[1];
-    }
-
-    return result;
-  }
-
   deletePost = (key) => {
     this.props.postRef.remove((err) => {
       if (err) console.log(err.message)
@@ -294,3 +253,44 @@ export default class Post extends Component {
     );
   }
 }
+
+// resizeImage = (dataUrl, type) => {
+//   const img = document.createElement("img");
+//   img.src = dataUrl;
+//   return new Promise((resolve, reject) => {
+//     img.onload = () => {
+//       // console.log(img.height);
+//       const canvas = document.createElement('canvas');
+//       const max = img.height > img.width ? img.height : img.width;
+//       if (max > 1000) {
+//         canvas.height = (img.height / max) * 1000;
+//         canvas.width = (img.width / max) * 1000;
+
+//         const context = canvas.getContext('2d');
+//         context.scale(1000 / max, 1000 / max);
+//         context.drawImage(img, 0, 0);
+//         // return canvas.toDataURL();
+//         resolve(canvas.toDataURL(type, 0.5));
+//       } else {
+//         // return dataUrl;
+//         resolve(dataUrl);
+//       }
+//     }
+//   });
+// };
+
+// base64MimeType = (encoded) => {
+//   var result = null;
+
+//   if (typeof encoded !== 'string') {
+//     return result;
+//   }
+
+//   var mime = encoded.match(/data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/);
+
+//   if (mime && mime.length) {
+//     result = mime[1];
+//   }
+
+//   return result;
+// }
