c39f27796b12a7d95f45c3b6f8ef5923bec7dd76
King Elisha <ek.chibueze@gmail.com>
2019-11-27 15:46:17 +0100

    profile pics

--- client/src/components/routes/Profile.js
+++ client/src/components/routes/Profile.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AuthNav from '../nav/AuthNav';
 import MainNav from '../nav/MainNav';
+import './Profile.scss';
 
 class Profile extends Component {
+  state = {
+    profile: {
+      avatar: '',
+      coverPhoto: '',
+    }
+  }
+
   render() {
     const hasProfilePic = false;
     const { user } = this.props.auth;
+    const { profile } = this.state;
 
     return (
       <div className="container">
@@ -16,7 +25,22 @@ class Profile extends Component {
           <MainNav user={user} />
 
           <div className="profile">
-            <h3 style={{ textAlign: "center", fontWeight: "500", padding: "1em 0" }}>Profile Coming Soon</h3>
+            <div className="pics">
+              <div className="cover">
+                {profile.coverPhoto ? (
+                  <img src={profile.coverPhoto} alt="Cover Photo" />
+                ) : (
+                    <div className="cover-placeholder"></div>
+                  )}
+              </div>
+              <div className="avatar">
+                {profile.avatar ? (
+                  <img src={profile.avatar} alt="Profile Picture" />
+                ) : (
+                    <div className="avatar-placeholder"></div>
+                  )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
