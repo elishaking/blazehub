ca14ecf4b61d9c7155de06739abe2ebe005bcec2
King Elisha <ek.chibueze@gmail.com>
2020-01-22 15:55:14 +0100

    firebase config

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -10,16 +10,7 @@ const sendInviteMail = require('../../utils/email');
 const validateSignupData = require('../../validation/signup');
 const validateSigninData = require('../../validation/signin');
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSEGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+const firebaseConfig = require('../../config/firebase');
 const firebaseApp = app.initializeApp(firebaseConfig);
 
 const dbRef = firebaseApp.database().ref();
