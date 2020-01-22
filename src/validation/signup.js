21cdc0409d8e3bc4a86af08298df1d36d06ae87c
King Elisha <ek.chibueze@gmail.com>
2020-01-22 13:54:40 +0100

    used utils

--- src/validation/signup.js
+++ src/validation/signup.js
@@ -1,5 +1,6 @@
 const Validator = require('validator').default;
 const isEmpty = require('./isEmpty');
+const ErrorMessage = require('../utils/errorMessage');
 
 const validateSignupData = (data) => {
   let errors = {};
@@ -10,27 +11,27 @@ const validateSignupData = (data) => {
   data.password = isEmpty(data.password) ? '' : data.password;
 
   if (Validator.isEmpty(data.firstName)) {
-    errors.firstName = 'Your first name is required';
+    errors.firstName = ErrorMessage.RequireError('first name');
   } else if (!Validator.isLength(data.firstName, { min: 2, max: 50 })) {
-    errors.firstName = 'Your first name must be between 2 and 50 characters';
+    errors.firstName = ErrorMessage.LengthError('first name', 2, 50);
   }
 
   if (Validator.isEmpty(data.lastName)) {
-    errors.lastName = 'Your last name is required';
+    errors.lastName = ErrorMessage.RequireError('last name');
   } else if (!Validator.isLength(data.lastName, { min: 2, max: 50 })) {
-    errors.lastName = 'Your last name must be between 2 and 50 characters';
+    errors.lastName = ErrorMessage.LengthError('last name', 2, 50);
   }
 
   if (Validator.isEmpty(data.email)) {
-    errors.email = "Your email is required"
+    errors.email = ErrorMessage.RequireError('email');
   } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Please enter a valid email"
+    errors.email = ErrorMessage.InvalidError('email');
   }
 
   if (Validator.isEmpty(data.password)) {
-    errors.password = 'Your password is required';
+    errors.password = ErrorMessage.RequireError('password');
   } else if (!Validator.isLength(data.password, { min: 4, max: 30 })) {
-    errors.password = 'Your password must be between 4 and 30 characters';
+    errors.password = ErrorMessage.LengthError('password', 4, 30);
   }
 
   return {
