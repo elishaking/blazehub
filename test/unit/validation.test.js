5db484937ffde78627c7c08fa52042d6f6ef82f9
King Elisha <ek.chibueze@gmail.com>
2020-01-22 14:00:37 +0100

    added more tests

--- test/unit/validation.test.js
+++ test/unit/validation.test.js
@@ -19,13 +19,25 @@ describe('Validation Unit Tests', () => {
     expect(isEmpty(new Array(10).fill(10))).toBe(false);
   });
 
-  it('validateSignupData() - should validate sign-up data', () => {
+  it('validateSignupData() - should validate empty data fields as inValid', () => {
     const data = {
       firstName: '',
       lastName: '',
       email: '',
       password: '',
     };
+
+    const validationResult = validateSignupData(data);
+
+    expect(validationResult.isValid).toBe(false);
+    expect(validationResult.errors.email).toEqual(ErrorMessage.RequireError('email'));
+    expect(validationResult.errors.firstName).toEqual(ErrorMessage.RequireError('first name'));
+    expect(validationResult.errors.lastName).toEqual(ErrorMessage.RequireError('last name'));
+    expect(validationResult.errors.password).toEqual(ErrorMessage.RequireError('password'));
+  });
+
+  it('validateSignupData() - should validate empty data as inValid', () => {
+    const data = {};
 
     const validationResult = validateSignupData(data);
 
