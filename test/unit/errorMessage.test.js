8a7d6ff0974ade783da3a46b9fb8c45c20bc5dd5
King Elisha <ek.chibueze@gmail.com>
2020-01-22 15:43:51 +0100

    ErrorMessage test suite

--- test/unit/errorMessage.test.js
+++ test/unit/errorMessage.test.js
@@ -6,3 +6,17 @@ describe('RequireError() Unit Tests', () => {
       .toEqual('Your <fieldName> is required')
   });
 });
+
+describe('InvalidError() Unit Tests', () => {
+  it('InvalidError() should return correct error message', () => {
+    expect(ErrorMessage.InvalidError('<fieldName>'))
+      .toEqual('Please enter a valid <fieldName>')
+  });
+});
+
+describe('LengthError() Unit Tests', () => {
+  it('LengthError() should return correct error message', () => {
+    expect(ErrorMessage.LengthError('<fieldName>', 3, 13))
+      .toEqual('Your <fieldName> must be between 3 and 13 characters')
+  });
+});
