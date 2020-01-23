32a9bfe37172e792ed3bb7f9806c618785f606aa
King Elisha <ek.chibueze@gmail.com>
2020-01-23 15:16:21 +0100

    create-user service added

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -7,6 +7,9 @@ require('firebase/database');
 const validateSignupData = require('../validation/signup');
 const validateSigninData = require('../validation/signin');
 
+const { createUser } = require('../services/users');
+const ResponseUtil = require('../utils/response');
+
 const dbRef = app.database().ref();
 
 /**
@@ -15,58 +18,11 @@ const dbRef = app.database().ref();
  * @param {express.Response} res 
  */
 const signupUser = (req, res) => {
-  const { isValid, errors } = validateSignupData(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  const userEmail = req.body.email;
-  const userKey = userEmail.replace(/\./g, "~").replace(/@/g, "~~");
-
-  const userRef = dbRef.child('users').child(userKey);
-  userRef
-    .once('value', (dataSnapshot) => {
-      if (dataSnapshot.exists()) {
-        errors.email = "Email already exists";
-        return res.status(400).json(errors);
-      }
-
-      bcrypt.genSalt(10, (err, salt) => {
-        if (err) console.error(err);
-        bcrypt.hash(req.body.password, salt, (err, hash) => {
-          if (err) return console.error(err);
-
-          const newUser = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: hash,
-          }
-          userRef
-            .set(newUser, (err) => {
-              if (err) return console.error(err);
-
-              // create default blazebot friend
-              dbRef.child('friends').child(userKey).set({
-                "blazebot": {
-                  name: "BlazeBot",
-
-                }
-              }, (err) => {
-                if (err) return console.error(err);
-
-                dbRef.child('profiles').child(userKey)
-                  .child('username').set(`${newUser.firstName.replace(/ /g, "")}.${newUser.lastName.replace(/ /g, "")}`.toLowerCase(), (err) => {
-                    if (err) return console.log(err);
-
-                    res.json({ success: true });
-                  });
-              });
-            });
-        });
-      });
-    });
+  createUser(req.body)
+    .then((responseData) => ResponseUtil.sendResponse(
+      res,
+      responseData
+    ));
 };
 
 /**
