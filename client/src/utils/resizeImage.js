99de136df2ee4f5638c2eace5d874e786cf76b6e
King Elisha <ek.chibueze@gmail.com>
2020-02-02 17:21:28 +0100

    resize image

---
+++ client/src/utils/resizeImage.js
@@ -0,0 +1,40 @@
+const resizeImage = (dataUrl, type) => {
+  const img = document.createElement("img");
+  img.src = dataUrl;
+  return new Promise((resolve, reject) => {
+    img.onload = () => {
+      // console.log(img.height);
+      const canvas = document.createElement('canvas');
+      const max = img.height > img.width ? img.height : img.width;
+      if (max > 1000) {
+        canvas.height = (img.height / max) * 1000;
+        canvas.width = (img.width / max) * 1000;
+
+        const context = canvas.getContext('2d');
+        context.scale(1000 / max, 1000 / max);
+        context.drawImage(img, 0, 0);
+        // return canvas.toDataURL();
+        resolve(canvas.toDataURL(type, 0.5));
+      } else {
+        // return dataUrl;
+        resolve(dataUrl);
+      }
+    }
+  });
+};
+
+const base64MimeType = (encoded) => {
+  var result = null;
+
+  if (typeof encoded !== 'string') {
+    return result;
+  }
+
+  var mime = encoded.match(/data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/);
+
+  if (mime && mime.length) {
+    result = mime[1];
+  }
+
+  return result;
+}
