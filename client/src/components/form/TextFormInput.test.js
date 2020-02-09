e9fecb22bb626f014f28faeae3de5df609f03749
King Elisha <ek.chibueze@gmail.com>
2020-02-09 16:46:48 +0100

    input tests

--- client/src/components/form/TextFormInput.test.js
+++ client/src/components/form/TextFormInput.test.js
@@ -23,6 +23,15 @@ const setUp = (props) => shallow(<TextFormInput {...props} />);
 const findByTestAttr = (component, attr) => component
   .find(`[data-test='${attr}']`);
 
+/**
+ * Find item with the specified attribute
+ * @param {*} component 
+ * @param {*} attrKey 
+ * @param {*} attrVal 
+ */
+const findByAttr = (component, attrKey, attrVal) => component
+  .find(`[${attrKey}='${attrVal}']`);
+
 describe('TextFormInput Component', () => {
   describe('With Props', () => {
     const props = {
@@ -34,13 +43,27 @@ describe('TextFormInput Component', () => {
       value: '',
     };
     const wrapper = setUp(props);
-    console.log(wrapper.debug());
 
     it('should render without errors', () => {
       const component = findByTestAttr(wrapper, 'textFormInputComponent');
 
       expect(component.length).toEqual(1);
     });
-  });
 
+    it('should have input', () => {
+      const input = wrapper.find('input');
+
+      expect(input.length).toEqual(1);
+    });
+
+    it('should have input with correct attributes', () => {
+      const input = wrapper.find('input');
+
+      expect(findByAttr(input, 'type', props.type).length).toEqual(1);
+      expect(findByAttr(input, 'name', props.name).length).toEqual(1);
+      expect(findByAttr(input, 'value', props.value).length).toEqual(1);
+      expect(findByAttr(input, 'placeholder', props.placeholder).length).toEqual(1);
+      expect(findByAttr(input, 'className', 'fill-parent error').length).toEqual(1);
+    });
+  });
 });
