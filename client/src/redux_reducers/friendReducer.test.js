b665e06ee78caa8787e819c7b378339322909572
King Elisha <ek.chibueze@gmail.com>
2020-02-09 19:02:54 +0100

    new friends state test

--- client/src/redux_reducers/friendReducer.test.js
+++ client/src/redux_reducers/friendReducer.test.js
@@ -1,3 +1,4 @@
+import { SET_FRIENDS } from '../redux_actions/types';
 import friendReducer from './friendReducer';
 
 describe('Friend Reducer', () => {
@@ -5,5 +6,26 @@ describe('Friend Reducer', () => {
     const newState = friendReducer(undefined, {});
 
     expect(newState).toEqual({});
+  });
+
+  it('should return new friends state', () => {
+    const friends = [
+      {
+        name: "John"
+      },
+      {
+        name: "James"
+      },
+    ];
+    const action = {
+      type: SET_FRIENDS,
+      payload: friends
+    };
+
+    const newState = friendReducer(undefined, action);
+    expect(newState).toEqual({
+      "0": friends[0],
+      "1": friends[1]
+    });
   });
 });
