b076416639c3b8e9c44748b2bacb0b7d43458306
King Elisha <ek.chibueze@gmail.com>
2020-02-14 12:15:54 +0100

    removed front-end

--- client/README.md
+++
@@ -1,3 +0,0 @@
-# BlazeHub FrontEnd React App
-
-A platform for connecting people: A place to chat, follow interesting conversions and be a part of a growing community
--- client/src/redux_actions/types.js
+++
@@ -1,9 +0,0 @@
-export const GET_ERRORS = 'GET_ERRORS';
-export const SET_CURRENT_USER = "SET_CURRENT_USER";
-
-export const SET_FRIENDS = "SET_FRIENDS";
-export const ADD_FRIEND = "ADD_FRIEND";
-
-export const SET_PROFILE_PIC = "SET_PROFILE_PIC";
-
-export const ADD_CHAT = "ADD_CHAT";
